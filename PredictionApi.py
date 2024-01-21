c70ac2d33921b11a852c4925cbdb0a9739ac1581
22009506-Rowena <134983507+22009506-Rowena@users.noreply.github.com>
2024-01-21 08:24:21 +0800

    Update PredictionApi.py

--- PredictionApi.py
+++ PredictionApi.py
@@ -66,8 +66,10 @@ def detect_objects():
         </form>
         '''
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+   if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
+
 
 
 
